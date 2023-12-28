023daea6e7b6403c1bc715228e9e36e3a11f570a
ShibilaTM <shibilaang@gmail.com>
2023-12-28 23:48:55 +0530

    first commit

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const morgan = require('morgan')
 const cors = require('cors')
 require('dotenv').config()
 require('./config/db')
+const path = require('path')
+
 const app = express()
 app.use(morgan('dev'))
 app.use(cors())
@@ -12,12 +14,16 @@ app.use(express.urlencoded({ extended: true }))
 
 const PORT = process.env.PORT
 
+app.use(express.static(path.join(__dirname,'build')))
 const userRoute = require('./routes/userRoutes')
 app.use('/user', userRoute)
 
 const formRoute = require('./routes/formRoutes')
 app.use('/form',formRoute)
 
+app.get('/*',function(req,res){
+    res.sendFile(path.join(__dirname,'build/index.html'))
+})
 app.listen(PORT,()=>{
     console.log(`server is listening on ${PORT}`)
 })
